0ab5ce60c658064469a05733f4b73e9e430da6ea
roman <roman.mariancik@gmail.com>
2022-10-20 01:04:45 +0200

    after arc

--- API/FaceRecAPI.py
+++ API/FaceRecAPI.py
@@ -6,6 +6,7 @@ from apscheduler.schedulers.background import BackgroundScheduler
 import dlib
 import numpy as np
 from django.utils import timezone
+from arcface import ArcFace
 
 import FaceRecognition.models as database
 import cv2
@@ -13,64 +14,30 @@ from django import db
 
 
 resnet = dlib.face_recognition_model_v1("models/dlib_face_recognition_resnet_model_v1.dat")
+arcface_model = ArcFace.ArcFace()
 detector = dlib.get_frontal_face_detector()
 sp = dlib.shape_predictor("models/shape_predictor_68_face_landmarks.dat")
 
-
-def load_image(filename):
-    img = cv2.imread(filename)
-    img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-    return img
+model = "arcface"
 
 
-def process_staff_descriptors_worker(name, file):
-    dir_path = os.path.join(os.path.dirname(__file__), "..") + "/media/"
-    full_path = dir_path + file
-    print("processing: ", full_path)
-    img = load_image(full_path)
-    face = detector(img, 1)
-    descriptor = None
-    if len(face) != 0:
-        landmarks = sp(img, face[0])
-        descriptor = np.array(resnet.compute_face_descriptor(img, landmarks))
-    else:
-        print("No face in picture {}".format(full_path))
-        database.Staff.objects.filter(name=name).delete()
-        print("record deleted from database")
-    return descriptor
+def dlib_dsc(face):
+    return np.array(resnet.compute_face_descriptor(face))
+
+def arcface_dsc(face):
+    return arcface_model.calc_emb(face)
 
 
-def process_staff_descriptors():
-    db.connections.close_all()
-    staff = database.Staff.objects.all()
-    names = list(staff.values_list('name', flat=True))
-    files = list(staff.values_list('file', flat=True))
-    pool = mp.Pool(processes=mp.cpu_count())
-    descriptors = (pool.starmap(process_staff_descriptors_worker, zip(names, files)))
-    descriptors = [item for item in descriptors if item is not None]
-    with open('staff_descriptors.pkl', 'wb') as outfile:
-        pickle.dump(descriptors, outfile, pickle.HIGHEST_PROTOCOL)
-    outfile.close()
-    print("descriptors of staff have been saved")
-
-    if len(descriptors) == 0:
-        print("No staff found")
-        return -1
-    return 0
+models = {"dlib": dlib_dsc,
+          "arcface": arcface_dsc}
 
 
-def get_descriptor(face_chip):
-    return np.array(resnet.compute_face_descriptor(face_chip))
+def get_descriptor(face, model):
+    return models[model](face)
 
 
-def load_staff_descriptors():
-    with open('staff_descriptors.pkl', 'rb') as infile:
-        staff_descriptors = pickle.load(infile)
-    print("Staff descriptors have been loaded")
-    return staff_descriptors
 
-
-def compare_all(descriptors, dsc, threshold):
+def dlib_compare(descriptors, dsc, threshold):
     # do for every comparison in the list comparisons
     comparisons = np.linalg.norm(descriptors - dsc, axis=1)
     for i in range(len(comparisons)):
@@ -79,31 +46,30 @@ def compare_all(descriptors, dsc, threshold):
     return False, -1
 
 
-def prune_logs(descriptors):
-    print("Pruning")
-    i = 0
-    while i < len(descriptors):
-        comparisons = np.linalg.norm(descriptors - descriptors[i], axis=1)
-        for j in range(len(comparisons)):
-            if 0.01 <= comparisons[j] <= 0.6:
-                print(j, comparisons[j])
-                descriptors.pop(j)
-        i += 1
-        print("length:", len(descriptors))
+def arcface_compare(descriptors, dsc, threshold):
+    # do for every comparison in the list comparisons
+    for i in range(len(descriptors)):
+        dst = arcface_model.get_distance_embeddings(descriptors[i], dsc)
+        if dst <= threshold:
+            return True, i
+    return False, -1
 
-    # for i in range(len(descriptors)):
-    #     comparisons = np.linalg.norm(descriptors - descriptors[i], axis=1)
-    #     for j in range(len(comparisons)):
-    #         if 0.01 <= comparisons[j] <= 0.6:
+comparison = {"dlib": dlib_compare,
+          "arcface": arcface_compare}
+
+
+def compare_all(descriptors, dsc, threshold, model):
+    return comparison[model](descriptors, dsc, threshold)
 
 
 def process_image(descriptors, staff_descriptors, staff, img):
+
     if len(detector(img, 1)) != 1:
         print("Invalid face in picture")
         return None, None
-    dsc = get_descriptor(img)
+    dsc = get_descriptor(img, model)
     if staff:
-        exists, idx = compare_all(staff_descriptors, dsc, 0.58)
+        exists, idx = compare_all(staff_descriptors, dsc, 0.58, model)
         if exists:
             # this person is staff
             print("person is staff")
@@ -112,7 +78,7 @@ def process_image(descriptors, staff_descriptors, staff, img):
         descriptors.append(dsc)
         # this is a new person and shared descriptors are empty
         return True, -1
-    exists, idx = compare_all(descriptors, dsc, 0.55)
+    exists, idx = compare_all(descriptors, dsc, 0.55, model)
     if exists:
         # this is an existing person
         return True, idx
@@ -160,6 +126,31 @@ def process_connection(c, shared_descriptors, shared_staff_descriptors, staff):
     exit(0)
 
 
+def load_staff_descriptors():
+    with open('staff_descriptors.pkl', 'rb') as infile:
+        staff_descriptors = pickle.load(infile)
+    print("Staff descriptors have been loaded")
+    return staff_descriptors
+
+
+def prune_logs(descriptors):
+    print("Pruning")
+    i = 0
+    while i < len(descriptors):
+        comparisons = np.linalg.norm(descriptors - descriptors[i], axis=1)
+        for j in range(len(comparisons)):
+            if 0.01 <= comparisons[j] <= 0.6:
+                print(j, comparisons[j])
+                descriptors.pop(j)
+        i += 1
+        print("length:", len(descriptors))
+
+    # for i in range(len(descriptors)):
+    #     comparisons = np.linalg.norm(descriptors - descriptors[i], axis=1)
+    #     for j in range(len(comparisons)):
+    #         if 0.01 <= comparisons[j] <= 0.6:
+
+
 def server_listener(s):
     manager = mp.Manager()
     shared_descriptors = manager.list()
@@ -188,6 +179,48 @@ def server_listener(s):
         p.start()
 
 
+
+def load_image(filename):
+    img = cv2.imread(filename)
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    return img
+
+def process_staff_descriptors_worker(name, file):
+    dir_path = os.path.join(os.path.dirname(__file__), "..") + "/media/"
+    full_path = dir_path + file
+    print("processing: ", full_path)
+    img = load_image(full_path)
+    face = detector(img, 1)
+    descriptor = None
+    if len(face) != 0:
+        landmarks = sp(img, face[0])
+        aligned_face = dlib.get_face_chip(img, landmarks, 150)
+        descriptor = get_descriptor(aligned_face, model)
+    else:
+        print("No face in picture {}".format(full_path))
+        database.Staff.objects.filter(name=name).delete()
+        print("record deleted from database")
+    return descriptor
+
+
+def process_staff_descriptors():
+    db.connections.close_all()
+    staff = database.Staff.objects.all()
+    names = list(staff.values_list('name', flat=True))
+    files = list(staff.values_list('file', flat=True))
+    pool = mp.Pool(processes=mp.cpu_count())
+    descriptors = (pool.starmap(process_staff_descriptors_worker, zip(names, files)))
+    descriptors = [item for item in descriptors if item is not None]
+    with open('staff_descriptors.pkl', 'wb') as outfile:
+        pickle.dump(descriptors, outfile, pickle.HIGHEST_PROTOCOL)
+    outfile.close()
+    print("descriptors of staff have been saved")
+
+    if len(descriptors) == 0:
+        print("No staff found")
+        return -1
+    return 0
+
 class FaceRecognition:
     def __init__(self):
 
@@ -212,10 +245,6 @@ class FaceRecognition:
 
         p = mp.Process(target=server_listener, args=(self.s, ))
         p.start()
-
-
-
-
 
 
 
